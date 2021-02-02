29c7cb6b8a5cb1fa2d13460018aa0c5e0bacc305
Yinshuang Gong <yinshuang@16financial.com>
2021-02-02 22:13:36 +0800

    0202

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from datetime import datetime
+
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+
+def showpost(request, post_pk):
+    try:
+        post = Post.objects.get(pk=post_pk)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        # return render(request, 'post.html', locals())
+        return redirect('/')
--- mblog/mblog/urls.py
+++ mblog/mblog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainsite.views import homepage, showpost
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage'),
+    path('post/<int:post_pk>/', showpost, name='detail'),
 ]
